de2f713fe59aa564ffeb69bebaa03177c66b192d
Michael de Leon <mike@mikes-air.home>
2015-05-10 17:32:53 -0700

    initial upload

---
+++ imageswap.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+<!-- hide script from old browsers
+function roll_over(img_name, img_src)
+   {
+   document[img_name].src = img_src;
+   }
+// end hiding script from old browsers -->
---
+++ style_tiplayer.js
@@ -0,0 +1,25 @@
+<!-- 
+/*
+Pleas leave this notice.
+DHTML tip message version 1.5 copyright Essam Gamal 2003.
+Home Pag: http://migoicons.tripod.com
+Email migoicons@hotmail.com
+Script featured on and can be found at Dynamic Drive (http://www.dynamicdrive.com)
+*/ 
+
+//Style format
+//Style[0]=["title text color","title bg color","title bg img","title text align","title font face",title font size,"text color","text bg color","text bg img","text align","text font face",
+//text font size,tip msg width,tip msg height,tip msg border size,"tip msg border color",tip msg text padding,tip msg popup effect,tip msg popup effect duration,tip msg transparency,
+//tip msg shadow type,"tip msg shadow color",tip msg behavior,tip msg position,tip msg x coord,tip msg y coord]
+Style[0]=["#FFFFFF","#666666","","","",1,"#000000","#FFFFFF","","","",1,200,,1,"#000000",3,24,0.5,,2,"#666666",,,-3,25]
+Text[0]=["&nbsp;D.E.Harvey Builders","3630 Westchase<br>P.O. Box 42008<br>Houston, TX 77242-2008<br>713.783.8710"]
+Text[1]=["&nbsp;Zann Commercial Brokerage, Inc.","2525 Bay Area Boulevard<br>P.O. Box 591159<br>Houston, TX 77259<br>281.280.8088"]
+Text[2]=["&nbsp;Redding Linden Burr","801 Travis<br>Suite 2000<br>Houston, TX 77002<br>713.237.9800"]
+Text[3]=["&nbsp;Ingenium, Inc.<br>&nbsp;Structural Engineers","7700 San Felipe<br>Suite 110<br>Houston, TX 77063-1611<br>713.339.9160"]
+Text[4]=["&nbsp;Gensler","711 Louisiana Street<br>Suite 300<br>Houston, TX 77002<br>713.844.0000"]
+Text[5]=["&nbsp;CEI Engineering Associates, Inc.","4800 Sugar Grove Boulevard<br>Suite 316<br>Stafford, TX 77477<br>713.339.9160"]
+
+var TipId="tiplayer" // should be the same as <div> tag's id
+var FiltersEnabled = 1 // should be the set as to 1 if your going to use visual effects if not set to 0
+mig_clay()
+//-->
